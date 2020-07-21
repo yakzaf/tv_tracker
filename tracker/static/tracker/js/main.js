9caf87a7f07a05a2dfe9ddc95119cd8beb239b94
yakzaf <zafar.yakubov77@gmail.com>
2020-07-21 17:30:59 +0300

    Centered the title with search bar on the index page. Removed unnecessary bootstrap table classes.

---
+++ tracker/static/tracker/js/main.js
@@ -0,0 +1,27 @@
+$(document).on('click', '.btn-add-remove', function(e) {
+    let value = $(this).val();
+    let formAction = $(this.form).attr('action');
+    console.log(formAction);
+    console.log(value);
+    $.ajax({
+        url: formAction,
+        type: "POST",
+        data: {
+            csrfmiddlewaretoken:$('input[name=csrfmiddlewaretoken]').val(),
+            alter_show_list: value
+        }
+    });
+    if (value === "add") {
+        $(this).removeClass("btn-success");
+        $(this).addClass("btn-danger");
+        $(this).val("remove");
+        $(this).html("Remove");
+    } else if (value === "remove") {
+        $(this).removeClass("btn-danger");
+        $(this).addClass("btn-success");
+        $(this).val("add");
+        $(this).html("Add");
+    }
+    e.preventDefault();
+    return false;
+});
