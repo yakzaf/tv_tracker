fe3c9d9fdfda0ef92f3af9dcd91ab89187b3adfa
yakzaf <zafar.yakubov77@gmail.com>
2020-11-06 20:28:38 +0100

    Closes #3 . Added pagination.

--- tracker/views.py
+++ tracker/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.http import HttpResponse
 from tracker.models import Show
 from user_auth.models import User
@@ -22,12 +23,15 @@ def index(request):
 def search_results(request):
     template = 'tracker/show_list.html'
     term = request.GET.get('q').strip()
+    page = request.GET.get('page', 1)
     vector = SearchVector('show_name')
     query = SearchQuery(term)
     if query.value:
-        shows = Show.objects.annotate(rank=SearchRank(vector, query)).filter(Q(show_name__icontains=term)).order_by('-rank')
+        shows = Show.objects.annotate(rank=SearchRank(vector, query)).filter(Q(show_name__icontains=term)).order_by(
+            '-rank')
     else:
         shows = Show.objects.none()
+
     if not shows.exists():
         shows_lookup = TvShows(term)
         results = shows_lookup.db_save()
@@ -40,8 +44,15 @@ def search_results(request):
             except User.DoesNotExist:
                 pass
             results.append(
-                {"added": added, "show_id": show.show_id, "show_name": show.show_name, "picture_url": show.picture_url,
-                 "service": show.service})
+                dict(added=added, show_id=show.show_id, show_name=show.show_name, picture_url=show.picture_url,
+                     service=show.service, kind=show.kind, overview=show.overview, year=show.year))
+        paginator = Paginator(results, 10)
+        try:
+            results = paginator.page(page)
+        except PageNotAnInteger:
+            results = paginator.page(1)
+        except EmptyPage:
+            results = paginator.page(paginator.num_pages)
 
     context = {'results': results}
 
@@ -61,15 +72,24 @@ def change_show_list(request, pk):
 
 
 def watchlist(request):
+    page = request.GET.get('page', 1)
     template = 'tracker/show_list.html'
     user_watchlist = Show.objects.filter(users__in=[request.user.id]).order_by('-user_shows__date_added')
     results = []
     for show in user_watchlist:
+        print(show.overview)
         results.append(
-            {"added": True, "show_id": show.show_id, "show_name": show.show_name, "picture_url": show.picture_url,
-             "service": show.service})
-    context = {
-        'results': results
-    }
+            dict(added=True, show_id=show.show_id, show_name=show.show_name, picture_url=show.picture_url,
+                 service=show.service, kind=show.kind, overview=show.overview, year=show.year))
+
+    paginator = Paginator(results, 10)
+    try:
+        results = paginator.page(page)
+    except PageNotAnInteger:
+        results = paginator.page(1)
+    except EmptyPage:
+        results = paginator.page(paginator.num_pages)
+
+    context = {'results': results}
 
     return render(request, template, context)
