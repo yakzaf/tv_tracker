e7717ebc886e848e675344c430f35aa4981211de
yakzaf <zafar.yakubov77@gmail.com>
2020-11-06 20:33:06 +0100

    Added OMDb API lookup for getting a release year, show/movie overview, and the media type (series/movie) information.

--- tracker/models.py
+++ tracker/models.py
@@ -9,7 +9,11 @@ from django.utils import timezone
 class Show(models.Model):
     show_id = models.AutoField(primary_key=True)
     show_name = models.CharField(max_length=100)
-    picture_url = models.TextField()
+    picture_url = models.TextField(blank=True, null=True)
+    # do I use datefield instead?
+    year = models.CharField(max_length=10, blank=True, null=True)
+    kind = models.CharField(max_length=10, null=True, default=None)
+    overview = models.TextField(blank=True, null=True)
     service = ArrayField(models.CharField(max_length=30))
 
     def __str__(self):
--- tracker/shows_db/show_lookup.py
+++ tracker/shows_db/show_lookup.py
@@ -28,30 +28,44 @@ class TvShows:
         self.term = term
 
     def db_save(self):
-        url = "https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup"
+        omdb_url = "http://www.omdbapi.com/"
+        utelly_url = "https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup"
         params = {'term': self.term.replace(' ', '-').lower()}
         headers = {
             'x-rapidapi-host': "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com",
             'x-rapidapi-key': config('UTELLY_API_KEY')
         }
+
         try:
-            r = requests.get(url, headers=headers, params=params)
+            r = requests.get(utelly_url, headers=headers, params=params)
             r.raise_for_status()
         except requests.exceptions.HTTPError as err:
             raise SystemExit(err)
-        response_json = r.json()
-        print(f'length = {len(response_json["results"])}')
+        utelly_r_json = r.json()
+        print(f'length = {len(utelly_r_json["results"])}')
 
-        if len(response_json["results"]) == 0:
+        if len(utelly_r_json["results"]) == 0:
             return
         results = []
         item_list = []
-        for result in response_json["results"]:
+        for result in utelly_r_json["results"]:
             show_name = result["name"]
+            params = {'t': show_name, 'plot': 'short', 'apikey': config('OMDB_API_KEY')}
+            try:
+                r = requests.get(omdb_url, params=params)
+                r.raise_for_status()
+            except requests.exceptions.HTTPError as err:
+                raise SystemExit(err)
+            omdb_r_json = r.json()
+            print(omdb_r_json)
+            year = omdb_r_json["Year"]
+            overview = omdb_r_json["Plot"]
+            kind = omdb_r_json["Type"].capitalize()
             shows = Show.objects.filter(show_name__exact=show_name)
             if shows.exists():
-                results.append({"show_id": shows[0].show_id, "show_name": shows[0].show_name,
-                                "picture_url": shows[0].picture_url, "service": shows[0].service})
+                results.append(
+                    dict(show_id=shows[0].show_id, show_name=shows[0].show_name, picture_url=shows[0].picture_url,
+                         year=year, kind=kind, overview=overview, service=shows[0].service))
                 continue
             request_services = result["locations"]
             pic = result["picture"]
@@ -60,13 +74,14 @@ class TvShows:
             for s in request_services:
                 services.append(s["display_name"])
 
-            item = {"show_name": show_name, "picture_url": picture_path, "service": services}
+            item = dict(show_name=show_name, picture_url=picture_path, year=year, kind=kind, overview=overview,
+                        service=services)
             item_list.append(Show(**item))
 
         if item_list:
             Show.objects.bulk_create(item_list)
-            results = [{"show_id": item_list[i].show_id, "show_name": item_list[i].show_name,
-                        "picture_url": item_list[i].picture_url, "service": item_list[i].service} for i in
-                       range(len(item_list))]
+            results = [dict(show_id=item_list[i].show_id, show_name=item_list[i].show_name,
+                            picture_url=item_list[i].picture_url, year=item_list[i].year, kind=item_list[i].kind,
+                            overview=item_list[i].overview, service=item_list[i].service) for i in range(len(item_list))]
 
         return results
